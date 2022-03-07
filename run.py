f51513d95ba0bdbf207fa4dbf6813bfb5abaae89
lubupang <31611588+lubupang@users.noreply.github.com>
2022-03-07 09:21:02 +0800

    Update run.py

--- run.py
+++ run.py
@@ -6,4 +6,4 @@ from wxcloudrun import app
 # 启动Flask Web服务
 if __name__ == '__main__':
     from waitress import serve
-        serve(app, host=sys.argv[1], port=sys.argv[2])
+    serve(app, host=sys.argv[1], port=sys.argv[2])
